303429b9688eb32c06395ce95fec1f010ca5722d
DeanWalsh <deanochambers@gmail.com>
2023-03-09 11:44:14 +0000

    Add task description & task modal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,42 +5,61 @@ import DailyTask from "./components/DailyTask";
 export interface ITask {
   id: number;
   taskName: string;
+  taskDescription: string;
   time: string;
 }
 
 function App() {
   const [task, setTask] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
   const [time, setTime] = useState<string>("00:00");
   const [daily, setDaily] = useState<ITask[]>([]);
+  const [selectedTask, setSelectedTask] = useState<ITask | null>(null);
   const keyId = useRef<number>(0);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     if (event.target.name === "task") {
       setTask(event.target.value);
+    } else if (event.target.name === "description") {
+      setDescription(event.target.value);
     } else {
       setTime(event.target.value);
     }
   };
 
   const addTask = (): void => {
-    const newTask = { id: keyId.current++, taskName: task, time: time };
-    if(task != "") {
-    setDaily([...daily, newTask]);
-    setTask("");
-    setTime("00:00");
+    const newTask = {
+      id: keyId.current++,
+      taskName: task,
+      taskDescription: description,
+      time: time,
+    };
+    if (task != "") {
+      setDaily([...daily, newTask]);
+      setTask("");
+      setDescription("");
+      setTime("00:00");
     }
   };
 
   const deleteTask = (taskId: number): void => {
-    setDaily(
-      daily.filter((task) => {
-        return task.id != taskId;
-      })
-    );
+    setDaily((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
   };
 
   // Add edit feature: const editTask = (taskNameEdit: undefined): void => {}
 
+  const handleTaskClick = (
+    taskId: number,
+    e: React.MouseEvent<HTMLDivElement>
+  ) => {
+    if (e.target instanceof HTMLButtonElement) {
+      return;
+    }
+    const task = daily.find((task) => task.id === taskId);
+    if (task) {
+      setSelectedTask(task);
+    }
+  };
 
   // Splitting up task time (string -> numbers) then sorting in list by time
   const compareTasks = (a: ITask, b: ITask) => {
@@ -79,6 +98,13 @@ function App() {
             onChange={handleChange}
           />
           <input
+            type="text"
+            value={description}
+            placeholder="Description..."
+            name="description"
+            onChange={handleChange}
+          />
+          <input
             type="time"
             value={time}
             placeholder="Time..."
@@ -93,11 +119,29 @@ function App() {
       <div className="daily-tasks">
         {sortedTasks.map((task: ITask) => {
           return (
-            <DailyTask key={task.id} task={task} deleteTask={deleteTask} />
-            // Add modal/pop-up for task description
+            <div
+              className="daily-tasks"
+              key={task.id}
+              onClick={(e) => handleTaskClick(task.id, e)}
+            >
+              <DailyTask key={task.id} task={task} deleteTask={deleteTask} />
+            </div>
           );
         })}
       </div>
+      {selectedTask && (
+        <div className="modal">
+          <div className="modal-container">
+            <button className="close" onClick={() => setSelectedTask(null)}>
+              X
+            </button>
+            <div className="modal-content">
+              <h2>{selectedTask.taskName}</h2>
+              <p>{selectedTask.taskDescription}</p>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
